60b9dc18c52cfe8d5c5a3809b962ab117b619bcf
Anuhya Samudrala <sgn.anuhya@gmail.com>
2024-05-06 22:20:41 -0400

    added cors

--- server/index.js
+++ server/index.js
@@ -13,22 +13,21 @@ import expenseRoute from "./routes/ExpenseRoute.js";
 import incomeRoute from "./routes/IncomeRoute.js";
 import BudgetsRoute from "./routes/BudgetsRoute.js";
 
-app.use(cors({
-    origin: '*', // Allow requests from any origin (not recommended for production)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
-    // allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
-}));
-
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', `*`);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
+    next();
+})
 
 app.listen(port, () => {
-    // console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 app.get("/", (req, res) => {
-    // console.log("Welcome to the server");
+    console.log("Welcome to the server");
 });
 
 connectDB();
